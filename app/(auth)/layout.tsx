59bc55374ef1c37d8ebff5c6795b964400b68c97
Giang <ngiang1309@gmail.com>
2025-12-10 15:30:16 +0700

    [update]: update dependencies, enhance global styles, and implement theme provider. Added new CSS variables for improved theming and integrated a toast notification system. Refactored the homepage to include user authentication features with sign-in and sign-out functionality.

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,51 @@
+import { buttonVariants } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="relative flex min-h-svh flex-col items-center justify-center">
+      <Link
+        href={"/"}
+        className={buttonVariants({
+          variant: "outline",
+          className: "absolute top-4 left-4",
+        })}
+      >
+        <ArrowLeft className="size-4" />
+        Back
+      </Link>
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <Link
+          href={"/"}
+          className="flex items-center gap-2 self-center font-medium"
+        >
+          <Image src="/logo.svg" alt="TunaLMS" width={32} height={32} />
+          TunaLMS.
+        </Link>
+        {children}
+
+        <div className="text-balance text-center text-xs text-muted-foreground">
+          By clicking continue, you agree to our{" "}
+          <Link
+            href={"/terms"}
+            className="underline hover:text-primary duration-300 transform-all"
+          >
+            Terms of Service
+          </Link>{" "}
+          and{" "}
+          <Link
+            href={"/privacy"}
+            className="underline hover:text-primary duration-300 transform-all"
+          >
+            Privacy Policy
+          </Link>
+          .
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AuthLayout;
---
+++ app/(auth)/verify-request/page.tsx
@@ -0,0 +1,102 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
+import { authClient } from "@/lib/auth-client";
+import { Loader2Icon } from "lucide-react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useState, useTransition } from "react";
+import { toast } from "sonner";
+
+const VerifyRequestPage = () => {
+  const router = useRouter();
+  const [otp, setOtp] = useState("");
+  const [emailPending, startEmailTransition] = useTransition();
+  const params = useSearchParams();
+  const email = params.get("email") as string;
+
+  const isOtpCompleted = otp.length === 6;
+
+  const verifyOtp = () => {
+    startEmailTransition(async () => {
+      await authClient.signIn.emailOtp({
+        email,
+        otp,
+        fetchOptions: {
+          onSuccess: () => {
+            toast.success("Email verified successfully");
+            router.push("/");
+          },
+          onError: (error) => {
+            toast.error(error.error.message);
+          },
+        },
+      });
+    });
+  };
+
+  return (
+    <Card className="w-full mx-auto">
+      <CardHeader className="text-center">
+        <CardTitle className="text-xl">Please check your email</CardTitle>
+        <CardDescription>
+          We&apos;ve sent a verification code to your email. Please check your
+          email and enter the code to verify your email.
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="space-y-6">
+        <div className="flex flex-col items-center space-y-2">
+          <InputOTP
+            maxLength={6}
+            className="gap-2"
+            value={otp}
+            onChange={(value) => setOtp(value)}
+          >
+            <InputOTPGroup>
+              <InputOTPSlot index={0} />
+              <InputOTPSlot index={1} />
+              <InputOTPSlot index={2} />
+            </InputOTPGroup>
+            <InputOTPSeparator />
+            <InputOTPGroup>
+              <InputOTPSlot index={3} />
+              <InputOTPSlot index={4} />
+              <InputOTPSlot index={5} />
+            </InputOTPGroup>
+          </InputOTP>
+          <p className="text-sm text-muted-foreground">
+            Enter the 6-digit code sent to your email
+          </p>
+        </div>
+        <Button
+          onClick={verifyOtp}
+          disabled={emailPending || !isOtpCompleted}
+          className="w-full"
+        >
+          {emailPending ? (
+            <>
+              <Loader2Icon className="size-4 animate-spin" />
+              <span>Verifying email...</span>
+            </>
+          ) : (
+            "Verify Account"
+          )}
+        </Button>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default VerifyRequestPage;
